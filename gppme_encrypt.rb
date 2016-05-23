99ca415dbf39688f3ec4457dda4ebfe5c873600f
Soumya Ray <soumya.ray@gmail.com>
2016-05-23 10:06:36 +0800

    fix rubocop style

--- gppme_encrypt.rb
+++ gppme_encrypt.rb
@@ -3,6 +3,6 @@ require 'gpgme'
 key = File.read('CE510594.asc')
 GPGME::Key.import key
 
-crypto = GPGME::Crypto.new :always_trust => true, armor: true
-result = crypto.encrypt "testing testing", recipients: "soumya.ray@gmail.com"
+crypto = GPGME::Crypto.new always_trust: true, armor: true
+result = crypto.encrypt 'testing testing', recipients: 'soumya.ray@gmail.com'
 puts result.to_s
